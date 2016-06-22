1f288d20b182b8bafd12289076d72583a4550848
Mostafa Anter <ga30000@gmail.com>
2016-06-22 16:18:37 +0200

    Initial commit

---
+++ app/src/main/java/perfect_apps/tutors/activities/SplashActivity.java
@@ -0,0 +1,63 @@
+package perfect_apps.tutors.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.akexorcist.localizationactivity.LocalizationActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import perfect_apps.tutors.R;
+import perfect_apps.tutors.store.TutorsPrefStore;
+import perfect_apps.tutors.utils.Constants;
+
+public class SplashActivity extends LocalizationActivity {
+    @Bind(R.id.splash_image_logo)
+    ImageView image0;
+    private Animation fade0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // set default language of activity
+        setDefaultLanguage("en");
+        setLanguage("ar");
+
+
+        setContentView(R.layout.content_splash);
+        ButterKnife.bind(this);
+
+        fade0 = AnimationUtils.loadAnimation(this, R.anim.fade_in_enter);
+
+        image0.startAnimation(fade0);
+        fade0.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(SplashActivity.this, HomeActivity.class)
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+                overridePendingTransition(R.anim.push_up_enter, R.anim.push_up_exit);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    private int checkFirstTimeOpenApp(){
+        return new TutorsPrefStore(this).getIntPreferenceValue(Constants.PREFERENCE_FIRST_TIME_OPEN_APP_STATE);
+    }
+
+    private void changeFirstTimeOpenAppState(){
+        new TutorsPrefStore(this).addPreference(Constants.PREFERENCE_FIRST_TIME_OPEN_APP_STATE, 1);
+    }
+}
---
+++ app/src/main/java/perfect_apps/tutors/utils/Constants.java
@@ -0,0 +1,19 @@
+package perfect_apps.tutors.utils;
+
+
+/**
+ * Created by mostafa on 14/04/16.
+ */
+public class Constants {
+    /**
+    * this section belong user preference
+    */
+    public static final String PREFERENCE_USER_EMAIL =  "email";
+    public static final String PREFERENCE_USER_NAME =  "name";
+    public static final String PREFERENCE_USER_IMAGE_URL = "image";
+    public static final String PREFERENCE_USER_AUTHENTICATION_STATE = "authenticated";
+    public static final String PREFERENCE_FIRST_TIME_OPEN_APP_STATE = "firstTimeState";
+
+
+
+}
