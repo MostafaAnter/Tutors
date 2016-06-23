ba06c6a965e2601090e04a1e48d6cd4016fe7d6a
Mostafa Anter <ga30000@gmail.com>
2016-06-23 15:46:03 +0200

    stableVersion

--- app/src/main/java/perfect_apps/tutors/activities/RegisterTeacherMembershipActivity.java
+++ app/src/main/java/perfect_apps/tutors/activities/RegisterTeacherMembershipActivity.java
@@ -1,6 +1,8 @@
 package perfect_apps.tutors.activities;
 
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,12 +12,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.akexorcist.localizationactivity.LocalizationActivity;
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import me.iwf.photopicker.PhotoPickerActivity;
+import me.iwf.photopicker.utils.PhotoPickerIntent;
 import perfect_apps.tutors.R;
 
 public class RegisterTeacherMembershipActivity extends LocalizationActivity {
@@ -24,6 +33,9 @@ public class RegisterTeacherMembershipActivity extends LocalizationActivity {
     @Bind(R.id.text3) TextView textView3;
     @Bind(R.id.editText1) EditText editText1;
     @Bind(R.id.button1) Button button1;
+    @Bind(R.id.image1) ImageView imageView1;
+
+    private static final int REQUEST_CODE = 1;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,5 +94,37 @@ public class RegisterTeacherMembershipActivity extends LocalizationActivity {
     }
 
     public void goToLogin(View view) {
+    }
+
+    public void pickPhoto(View view) {
+        PhotoPickerIntent intent = new PhotoPickerIntent(RegisterTeacherMembershipActivity.this);
+        intent.setPhotoCount(1);
+        intent.setShowCamera(true);
+        intent.setShowGif(true);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            if (data != null) {
+                ArrayList<String> photos =
+                        data.getStringArrayListExtra(PhotoPickerActivity.KEY_SELECTED_PHOTOS);
+                Uri uri = Uri.fromFile(new File(photos.get(0)));
+                setSelectedPhotoInsideCircleShap(uri);
+            }
+        }
+    }
+
+    private void setSelectedPhotoInsideCircleShap(Uri uri){
+        Glide.with(this)
+                .load(uri)
+                .centerCrop()
+                .thumbnail(0.1f)
+                .placeholder(R.drawable.__picker_ic_photo_black_48dp)
+                .error(R.drawable.__picker_ic_broken_image_black_48dp)
+                .into(imageView1);
     }
 }
