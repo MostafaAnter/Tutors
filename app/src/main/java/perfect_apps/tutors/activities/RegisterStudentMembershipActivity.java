46303a6e2eaac283ac65aa99f007da162c9faf4c
Mostafa Anter <ga30000@gmail.com>
2016-06-29 14:17:30 +0200

    stableVersion

--- app/src/main/java/perfect_apps/tutors/activities/RegisterStudentMembershipActivity.java
+++ app/src/main/java/perfect_apps/tutors/activities/RegisterStudentMembershipActivity.java
@@ -1,31 +1,155 @@
 package perfect_apps.tutors.activities;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.akexorcist.localizationactivity.LocalizationActivity;
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import me.iwf.photopicker.PhotoPickerActivity;
+import me.iwf.photopicker.utils.PhotoPickerIntent;
 import perfect_apps.tutors.R;
 
-public class RegisterStudentMembershipActivity extends AppCompatActivity {
+public class RegisterStudentMembershipActivity extends LocalizationActivity {
+    @Bind(R.id.text1) TextView textView1;
+    @Bind(R.id.text2) TextView textView2;
+    @Bind(R.id.text3) TextView textView3;
+    @Bind(R.id.text4) TextView textView4;
 
+    @Bind(R.id.editText1) EditText editText1;
+    @Bind(R.id.editText2) EditText editText2;
+    @Bind(R.id.editText3) EditText editText3;
+
+    @Bind(R.id.button1) Button button1;
+    @Bind(R.id.button2) Button button2;
+    @Bind(R.id.image1) ImageView imageView1;
+
+
+    private static final int REQUEST_CODE = 1;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_student_membership);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ButterKnife.bind(this);
+        setToolbar();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        changeTextFont();
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.arrowleft_white);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavUtils.navigateUpFromSameTask(RegisterStudentMembershipActivity.this);
+                overridePendingTransition(R.anim.push_left_enter, R.anim.push_left_exit);
+            }
+        });
+
+        /*
+        * hide title
+        * */
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        //toolbar.setNavigationIcon(R.drawable.ic_toolbar);
+        toolbar.setTitle("");
+        toolbar.setSubtitle("");
+        //toolbar.setLogo(R.drawable.ic_toolbar);
+
+        /*
+        * change font of title
+        * */
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+        Typeface sharknyFont = Typeface.createFromAsset(getAssets(), "fonts/bold.ttf");
+        mTitle.setTypeface(sharknyFont);
+
+    }
+
+    // set on back button pressed
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.push_left_enter, R.anim.push_left_exit);
+        finish();
+    }
+
+    private void changeTextFont(){
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/normal.ttf");
+        Typeface fontBold = Typeface.createFromAsset(getAssets(), "fonts/bold.ttf");
+        textView1.setTypeface(font);
+        textView2.setTypeface(font);
+        textView4.setTypeface(font);
+        editText1.setTypeface(font);
+        editText2.setTypeface(font);
+        editText3.setTypeface(font);
+        button1.setTypeface(font);
+        button2.setTypeface(fontBold);
+        textView3.setTypeface(fontBold);
+    }
+
+    public void goToLogin(View view) {
+        Intent intent = new Intent(this, LoginStudentActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.push_right_enter, R.anim.push_right_exit);
+        finish();
+
+    }
+
+    public void pickPhoto(View view) {
+        PhotoPickerIntent intent = new PhotoPickerIntent(RegisterStudentMembershipActivity.this);
+        intent.setPhotoCount(1);
+        intent.setShowCamera(true);
+        intent.setShowGif(true);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            if (data != null) {
+                ArrayList<String> photos =
+                        data.getStringArrayListExtra(PhotoPickerActivity.KEY_SELECTED_PHOTOS);
+                Uri uri = Uri.fromFile(new File(photos.get(0)));
+                setSelectedPhotoInsideCircleShap(uri);
+            }
+        }
+    }
+
+    private void setSelectedPhotoInsideCircleShap(Uri uri){
+        Glide.with(this)
+                .load(uri)
+                .centerCrop()
+                .thumbnail(0.1f)
+                .placeholder(R.drawable.__picker_ic_photo_black_48dp)
+                .error(R.drawable.__picker_ic_broken_image_black_48dp)
+                .into(imageView1);
+    }
+
+    public void registerNewUser(View view) {
+    }
 }
