74cda6217a5c84dc55b15ab9cc1f2fbb0bb01096
Mostafa Anter <ga30000@gmail.com>
2016-07-20 08:43:50 +0200

    stableVersion

---
+++ app/src/main/java/perfect_apps/tutors/fragments/RateInfoFragment.java
@@ -0,0 +1,96 @@
+package perfect_apps.tutors.fragments;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import perfect_apps.tutors.R;
+import perfect_apps.tutors.activities.LoginStudentActivity;
+import perfect_apps.tutors.activities.RegisterStudentMembershipActivity;
+
+/**
+ * Created by mostafa on 07/07/16.
+ */
+public class RateInfoFragment extends DialogFragment implements View.OnClickListener {
+    int mNum;
+
+
+    @Bind(R.id.text1)TextView textView1;
+    @Bind(R.id.text2)TextView textView2;
+    @Bind(R.id.text3)TextView textView3;
+    @Bind(R.id.text4)TextView textView4;
+    @Bind(R.id.text5)TextView textView5;
+    @Bind(R.id.text6)TextView textView6;
+
+
+    @Bind(R.id.closeDialog) ImageView close;
+
+    /**
+     * Create a new instance of MyDialogFragment, providing "num"
+     * as an argument.
+     */
+    static RateInfoFragment newInstance(int num) {
+        RateInfoFragment f = new RateInfoFragment();
+
+        // Supply num input as an argument.
+        Bundle args = new Bundle();
+        args.putInt("num", num);
+        f.setArguments(args);
+
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Pick a style based on the num.
+        int style = DialogFragment.STYLE_NO_TITLE, theme = android.R.style.Theme_Holo_Light_Dialog;
+        setStyle(style, theme);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_dialog, container, false);
+        ButterKnife.bind(this, v);
+        changeTextFont();
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        close.setOnClickListener(this);
+
+    }
+
+    private void changeTextFont(){
+        Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "fonts/normal.ttf");
+        Typeface fontBold = Typeface.createFromAsset(getActivity().getAssets(), "fonts/bold.ttf");
+        textView1.setTypeface(fontBold);
+        textView2.setTypeface(font);
+        textView3.setTypeface(font);
+        textView4.setTypeface(font);
+        textView5.setTypeface(font);
+        textView6.setTypeface(font);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.closeDialog:
+                dismiss();
+                break;
+        }
+    }
+}
